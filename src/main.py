266a4295e1c04ffc9fd4402e8430c29301f4b849
creativemario <creativemario@users.noreply.github.com>
2024-10-16 11:07:31 +0100

    Pythonic naming conventions

--- src/main.py
+++ src/main.py
@@ -7,34 +7,34 @@ fire = DigitalOutputDevice(18)
 fire.off()
 
 # global control for fire enable
-icanhasfire = False
+I_CAN_HAS_FIRE = False
 
 # global fire duration
-fire_duration_ms = 3000
+FIRE_DURATION_MS = 3000
 # global lockout duration
-lockout_duration_ms = 10000
+LOCKOUT_DURATION_MS = 10000
 
 # setup the LED on GPIO 19 (this is a relay pin for testing, use a real LED on a different pin IRL).
 led = DigitalLED(19)
 
 # global control for tracking LED state
-led_blinking = False
+LED_BLINKING = False
 
 # Setup the button on GPIO 15
 button = Button(15)
 
 # method for handling button press
-def checkandtriggerfire():
-    global icanhasfire
-    global led_blinking
-    if icanhasfire:
+def check_and_trigger_fire():
+    global I_CAN_HAS_FIRE
+    global LED_BLINKING
+    if I_CAN_HAS_FIRE:
         led.off()
-        led_blinking = False
-        icanhasfire = False
+        LED_BLINKING = False
+        I_CAN_HAS_FIRE = False
         print("flame on")
         fire.on()
-        print("waiting", fire_duration_ms, "ms")
-        sleep(fire_duration_ms)
+        print("waiting", FIRE_DURATION_MS, "ms")
+        sleep(FIRE_DURATION_MS)
         print("flame off")
         fire.off()
 
@@ -42,17 +42,17 @@ def checkandtriggerfire():
 # Do the loop
 while(True):
     # Blink LED if lockout disabled, and not already blinking
-    if(icanhasfire and not led_blinking):
+    if(I_CAN_HAS_FIRE and not LED_BLINKING):
         led.blink()
-        led_blinking = True
+        LED_BLINKING = True
     
     # if the button is being pressed and you are allowed to, do the fire
-    if(icanhasfire and button.is_pressed):
-        checkandtriggerfire()
+    if(I_CAN_HAS_FIRE and button.is_pressed):
+        check_and_trigger_fire()
     
     # handle lockout
-    if(not icanhasfire):
-        print("lockout for", lockout_duration_ms, "ms")
-        sleep(lockout_duration_ms)
+    if(not I_CAN_HAS_FIRE):
+        print("lockout for", LOCKOUT_DURATION_MS, "ms")
+        sleep(LOCKOUT_DURATION_MS)
         print("enabling fire")
-        icanhasfire = True
+        I_CAN_HAS_FIRE = True
